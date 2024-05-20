56eff231318d83758984967e199b3a8e0495032a
karkisarkar <prabin@nydoz.com>
2024-05-20 15:12:12 +0545

    thank-you page for partner

--- themes/gowithfund/template-thankyou.php
+++ themes/gowithfund/template-thankyou.php
@@ -1,33 +1,36 @@
 <?php
-        /*
-        Template Name: Thank You Page
-        */
-        get_header();
-        ?>
+/*
+Template Name: Thank You Page
+*/
+get_header();
+?>
 
-        <div id="primary" class="content-area">
-            <main id="main" class="site-main">
-                <section class="thank-you">
-                    <header class="page-header">
-                        <h1 class="page-title" style="text-align: center;"><?php esc_html_e('Thank You', 'krowd'); ?></h1>
-                    </header>
-                    <div class="page-content">
-                        <p style="text-align: center;"><?php esc_html_e('Thank you for your request on becoming a partner. We will get back to you soon.', 'krowd'); ?></p>
-                    </div>
+<div id="primary" class="content-area">
+    <main id="main" class="site-main">
+        <section class="thank-you">
+            <header class="page-header">
+                <h1 class="page-title" style="text-align: center;"><?php esc_html_e('Thank You', 'krowd'); ?></h1>
+            </header>
+            <div class="page-content">
+                <p style="text-align: center;"><?php esc_html_e('Thank you for your request on becoming a partner. We will get back to you soon.', 'krowd'); ?></p>
+                <div class="not-found-home text-center">
+                    <a href="https://dev.gowithfund.com/"><i class="far fa-arrow-alt-circle-left"></i>Back Homepage</a>
+                </div>
+            </div>
 
-                </section>
-            </main>
-        </div>
+        </section>
+    </main>
+</div>
 
-        <?php
-        get_footer();
-        ?>
+<?php
+get_footer();
+?>
         
 <script>
 // JavaScript to redirect to home page when the Thank You page is refreshed
 window.onload = function() {
     if(performance.navigation.type == 1) {
-        window.location.href = '<?php echo esc_url( home_url( '/' ) ); ?>';
+        window.location.href = '<?php echo esc_url( home_url( '/become-a-partner' ) ); ?>';
     }
 }
 </script>

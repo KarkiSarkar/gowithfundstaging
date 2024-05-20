57d3e0fcde3a7eaa09ca74d958aa340aaebf2cc3
karkisarkar <prabin@nydoz.com>
2024-05-20 15:16:12 +0545

    thank-you page for partner

--- themes/gowithfund/template-thankyou.php
+++ themes/gowithfund/template-thankyou.php
@@ -8,19 +8,11 @@ get_header();
 <div id="primary" class="content-area">
     <main id="main" class="site-main">
         <section class="thank-you">
-            <header class="page-header">
-                <h1 class="page-title" style="text-align: center;"><?php esc_html_e('Thank You', 'krowd'); ?></h1>
-            </header>
-            <div class="page-content">
-                <p style="text-align: center;"><?php esc_html_e('Thank you for your request on becoming a partner. We will get back to you soon.', 'krowd'); ?></p>
-                <div class="not-found-home text-center">
-                    <a href="https://dev.gowithfund.com/"><i class="far fa-arrow-alt-circle-left"></i>Back Homepage</a>
-                </div>
-            </div>
+            
             <div class=" text-center">
-			<div class="not-found-title"><h1><span>404</span></h1></div>
-			<div class="not-found-subtitle">Page Not Found</div>
-			<div class="not-found-desc">The page requested couldn't be found. This could be a spelling error in the URL or a removed page.</div> 
+			<div class="not-found-title"><h1><span><?php esc_html_e('Thank You', 'krowd'); ?></span></h1></div>
+			<!-- <div class="not-found-subtitle">Page Not Found</div> -->
+			<div class="not-found-desc"><?php esc_html_e('Thank you for your request on becoming a partner. We will get back to you soon.', 'krowd'); ?></div> 
 
 			<div class="not-found-home text-center">
 				<a href="https://dev.gowithfund.com/"><i class="far fa-arrow-alt-circle-left"></i>Back Homepage</a>

2f1312ed9850c8dfd3ba97a6d013ab58b917a97b
karkisarkar <prabin@nydoz.com>
2024-05-21 14:21:02 +0545

    template for partnership single page

--- themes/gowithfund/template-partnership-page.php
+++ themes/gowithfund/template-partnership-page.php
@@ -15,18 +15,18 @@ get_header();
                 <h1 class="page-title"><?php the_title(); ?></h1>
             </header>
             <div style="display: flex;">
-            <div class="page-content" style="width: 48%;">
-                <p><?php the_field('description');?></p>
-            </div>
-            <div style="width: 48%;">
-            <?php
-                if ( has_post_thumbnail() ) {
-                    the_post_thumbnail('full'); // 'full' can be changed to 'thumbnail', 'medium', 'large', etc. based on your needs
-                }
-                ?>
+                <div class="page-content" style="width: 48%;">
+                    <p><?php the_field('description');?></p>
+                </div>
+                <div style="width: 48%;">
+                <?php
+                    if ( has_post_thumbnail() ) {
+                        the_post_thumbnail('full'); // 'full' can be changed to 'thumbnail', 'medium', 'large', etc. based on your needs
+                    }
+                    ?>
 
+                </div>
             </div>
-            
             <div style="background: #00A9A5; width: 70%; padding-top: 3rem;">
                 <?php echo do_shortcode('[custom_contact_form]'); ?>
             </div>

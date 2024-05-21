30d42ca34ad62f92b98502ccad9707cfd9362162
karkisarkar <prabin@nydoz.com>
2024-05-21 10:58:11 +0545

    meta pixel setup lead form

--- plugins/Partnership Plugin/partnership.php
+++ plugins/Partnership Plugin/partnership.php
@@ -29,7 +29,24 @@ function custom_contact_form_shortcode() {
             margin-left: 15px;
         }
     </style>
-    
+    <!-- Meta Pixel Code -->
+<script>
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '1443812046255824'); 
+  fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=1443812046255824&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Meta Pixel Code -->
+
     <form id="custom-contact-form" method="post" action="" enctype="multipart/form-data">
        <input type="hidden" name="custom_contact_form_submit" value="1">
             <div class="col-sm-12 col-xs-12">

bdf33d00be66ad7c3c34843a18fbc4381ff2c566
karkisarkar <prabin@nydoz.com>
2024-05-21 13:58:50 +0545

    template for partnership single page

--- themes/gowithfund/template-partnership-page.php
+++ themes/gowithfund/template-partnership-page.php
@@ -11,7 +11,7 @@ get_header();
                 <h1 class="page-title"><?php the_title(); ?></h1>
             </header>
             <div class="page-content">
-                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
+                <p><?php //the_field('description');?></p>
                 <h4>Key Feature</h4>
                 <ul>
                     <li>Key 1</li>

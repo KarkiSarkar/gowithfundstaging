466f286f36f8f4f6d73b453e0a3c6ee168ba8180
karkisarkar <prabin@nydoz.com>
2024-05-21 12:48:19 +0545

    template for partnership single page

--- themes/gowithfund/template-partnership-page.php
+++ themes/gowithfund/template-partnership-page.php
@@ -4,21 +4,14 @@ Template Name: Partner Single Page
 */
 get_header();
 ?>
-<style>
-    .buttonhovercss > a{
-            padding: 10px 30px 10px 30px;
-            border-radius: 20px;
-            background: #00A9A5!important;
-    }
-</style>
 <div id="primary" class="content-area">
     <main id="main" class="site-main">
-        <section class="thank-you">
+        <section>
             <header class="page-header">
-                <h1 class="page-title" style="text-align: center; font-size: 60px; color: #00A9A5; padding-top: 3rem;"><?php esc_html_e('Title', 'krowd'); ?></h1>
+                <h1 class="page-title"><?php esc_html_e('Title', 'krowd'); ?></h1>
             </header>
             <div class="page-content">
-                <p style="text-align: center;"><?php esc_html_e('Description ', 'krowd'); ?></p>
+                <p><?php esc_html_e('Description ', 'krowd'); ?></p>
                 <h4>Key Feature</h4>
                 <ul>
                     <li>Key 1</li>

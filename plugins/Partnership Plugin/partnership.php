01eda1229a60098e223304c86414d98e01454180
karkisarkar <prabin@nydoz.com>
2024-05-21 11:11:48 +0545

    meta pixel setup lead form

--- plugins/Partnership Plugin/partnership.php
+++ plugins/Partnership Plugin/partnership.php
@@ -5,7 +5,27 @@ Description: Plugin to generate a custom contact form and send emails upon submi
 Version: 1.0
 Author: Nydoz Team
 */
-
+function add_facebook_tracking_code() {
+    ?>
+     <script>
+      !function(f,b,e,v,n,t,s)
+       {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+       n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+       if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+       n.queue=[];t=b.createElement(e);t.async=!0;
+       t.src=v;s=b.getElementsByTagName(e)[0];
+       s.parentNode.insertBefore(t,s)}(window, document,'script',
+       'https://connect.facebook.net/en_US/fbevents.js');
+       fbq('init', '1443812046255824'); 
+       fbq('track', 'PageView');
+     </script>
+     <noscript><img height="1" width="1" style="display:none"
+       src="https://www.facebook.com/tr?id=1443812046255824&ev=PageView&noscript=1"
+     /></noscript>
+     <?php
+   }
+   add_action('wp_head', 'add_facebook_tracking_code');
+   add_action('wp_footer', 'add_facebook_tracking_code');
 // Enqueue scripts and styles
 function custom_contact_form_scripts() {
     // Enqueue scripts and styles here if needed
@@ -30,21 +50,7 @@ function custom_contact_form_shortcode() {
         }
     </style>
     <!-- Meta Pixel Code -->
-<script>
-  !function(f,b,e,v,n,t,s)
-  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-  n.queue=[];t=b.createElement(e);t.async=!0;
-  t.src=v;s=b.getElementsByTagName(e)[0];
-  s.parentNode.insertBefore(t,s)}(window, document,'script',
-  'https://connect.facebook.net/en_US/fbevents.js');
-  fbq('init', '1443812046255824'); 
-  fbq('track', 'PageView');
-</script>
-<noscript><img height="1" width="1" style="display:none"
-  src="https://www.facebook.com/tr?id=1443812046255824&ev=PageView&noscript=1"
-/></noscript>
+
 <!-- End Meta Pixel Code -->
 
     <form id="custom-contact-form" method="post" action="" enctype="multipart/form-data">

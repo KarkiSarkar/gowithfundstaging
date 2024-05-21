b77b68b318bf16891085df81027867452657d681
karkisarkar <prabin@nydoz.com>
2024-05-21 14:01:55 +0545

    template for partnership single page

--- themes/gowithfund/template-partnership-page.php
+++ themes/gowithfund/template-partnership-page.php
@@ -12,13 +12,7 @@ get_header();
             </header>
             <div class="page-content">
                 <p><?php the_field('description');?></p>
-                <h4>Key Feature</h4>
-                <ul>
-                    <li>Key 1</li>
-                    <li>Key 1</li>
-                    <li>Key 1</li>
-                    <li>Key 1</li>
-                </ul>
+                
             </div>
             <div style="background: #00A9A5;">
                 <?php echo do_shortcode('[custom_contact_form]'); ?>

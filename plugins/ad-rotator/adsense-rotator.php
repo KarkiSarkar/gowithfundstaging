47b2b95d50b954fdc94ad0131429a7b26623497e
karkisarkar <prabin@nydoz.com>
2024-05-28 17:06:21 +0545

    test

--- plugins/ad-rotator/adsense-rotator.php
+++ plugins/ad-rotator/adsense-rotator.php
@@ -10,12 +10,23 @@ Author: Nydoz Team
 add_action('admin_menu', 'adsense_rotator_menu');
 function adsense_rotator_menu() {
     add_options_page('AdSense Rotator Settings', 'AdSense Rotator', 'manage_options', 'adsense-rotator', 'adsense_rotator_settings_page');
+    add_options_page('ads.txt Editor', 'ads.txt Editor', 'manage_options', 'ads-txt-editor', 'ads_txt_editor_page');
 }
 
 // Register settings
 add_action('admin_init', 'adsense_rotator_settings');
 function adsense_rotator_settings() {
-    register_setting('adsense-rotator-settings-group', 'adsense_rotator_ad_units');
+    register_setting('adsense-rotator-settings-group', 'adsense_rotator_ad_units', 'sanitize_ad_units');
+}
+
+// Sanitize the input
+function sanitize_ad_units($input) {
+    if (is_array($input)) {
+        foreach ($input as $key => $value) {
+            $input[$key] = sanitize_text_field($value);
+        }
+    }
+    return $input;
 }
 
 // Settings page HTML
@@ -27,72 +38,90 @@ function adsense_rotator_settings_page() {
             <?php settings_fields('adsense-rotator-settings-group'); ?>
             <?php do_settings_sections('adsense-rotator-settings-group'); ?>
 
-            <table class="form-table">
+            <table class="form-table" id="adsense-rotator-ad-units">
                 <tr valign="top">
                     <th scope="row">Ad Units</th>
-                    <td>
-                        <textarea name="adsense_rotator_ad_units" rows="10" cols="50" class="large-text code"><?php echo esc_textarea(get_option('adsense_rotator_ad_units')); ?></textarea>
-                        <p>Enter the ad units in JSON format. Example:
-                            <pre>
-[
-    {
-        "client_id": "ca-pub-xxxxxxx",
-        "name": "Sponsered1"
-    },
-    {
-        "client_id": "ca-pub-yyyyyyyy",
-        "name": "Sponsered2"
-    },
-    {
-        "client_id": "ca-pub-zzzzzzzz",
-        "name": "Sponsered3"
-    }
-]
-                            </pre>
-                        </p>
+                    <td id="ad-units-container">
+                        <?php
+                        $ad_units = get_option('adsense_rotator_ad_units', array());
+                        if (!is_array($ad_units)) {
+                            $ad_units = array();
+                        }
+                        if (!empty($ad_units)) {
+                            foreach ($ad_units as $index => $ad_unit) {
+                                ?>
+                                <div class="ad-unit">
+                                    <input type="text" name="adsense_rotator_ad_units[]" value="<?php echo esc_attr($ad_unit); ?>" class="large-text" />
+                                    <button type="button" class="button remove-ad-unit">Remove</button>
+                                </div>
+                                <?php
+                            }
+                        } else {
+                            ?>
+                            <div class="ad-unit">
+                                <input type="text" name="adsense_rotator_ad_units[]" value="" class="large-text" />
+                                <button type="button" class="button remove-ad-unit">Remove</button>
+                            </div>
+                            <?php
+                        }
+                        ?>
                     </td>
                 </tr>
             </table>
 
+            <button type="button" class="button" id="add-ad-unit">Add Ad Unit</button>
+
             <?php submit_button(); ?>
         </form>
     </div>
+    <script>
+    document.addEventListener('DOMContentLoaded', function () {
+        var container = document.getElementById('ad-units-container');
+        var addButton = document.getElementById('add-ad-unit');
+
+        addButton.addEventListener('click', function () {
+            var div = document.createElement('div');
+            div.className = 'ad-unit';
+            div.innerHTML = '<input type="text" name="adsense_rotator_ad_units[]" value="" class="large-text" /> <button type="button" class="button remove-ad-unit">Remove</button>';
+            container.appendChild(div);
+        });
+
+        container.addEventListener('click', function (e) {
+            if (e.target && e.target.classList.contains('remove-ad-unit')) {
+                e.target.parentElement.remove();
+            }
+        });
+    });
+    </script>
     <?php
 }
 
-// Function to get rotated AdSense ad units from the database
+// Function to get the ad units from the database
 function get_rotated_ad_units() {
     $ad_units = get_option('adsense_rotator_ad_units');
-    if ($ad_units) {
-        return json_decode($ad_units, true);
+    if (!is_array($ad_units)) {
+        $ad_units = array();
     }
-    return array();
+    return $ad_units;
 }
 
-// Function to rotate AdSense ad units with names
+// Function to rotate AdSense ad units
 function rotate_named_adsense_ads() {
-    // if (!isset($_SESSION)) {
-    //     session_start();
-    // }
-
-    // Check if the session variable is set
-    if (!isset($_SESSION['selected_ad_unit'])) {
-        // Randomly select an ad unit
-        $_SESSION['selected_ad_unit'] = array_rand(get_rotated_ad_units());
-    }
-
-    // Get the selected ad unit
     $ad_units = get_rotated_ad_units();
+
+    // Check if there are any ad units
     if (empty($ad_units)) {
-        return;
+        return; // No ad units to display
     }
-    $selected_ad = $ad_units[$_SESSION['selected_ad_unit']];
+
+    // Randomly select an ad unit
+    $selected_ad = $ad_units[array_rand($ad_units)];
 
     // Output the ad unit
     if (!is_user_logged_in()) {
         ?>
-        <p><?php echo esc_attr($selected_ad['client_id']); ?></p>
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=<?php echo esc_attr($selected_ad['client_id']); ?>&amp;cachebuster=<?php echo time(); ?>" crossorigin="anonymous"></script>
+        <p><?php echo esc_attr($selected_ad); ?></p>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-<?php echo esc_attr($selected_ad); ?>&amp;cachebuster=<?php echo time(); ?>" crossorigin="anonymous"></script>
         <?php
     }
 }
@@ -108,7 +137,7 @@ function insert_ads_after_paragraph($content) {
         // Split content into paragraphs
         $paragraphs = explode("</p>", $content);
 
-        // Insert shortcode after every second paragraph
+        // Insert shortcode after every third paragraph
         for ($i = 2; $i < count($paragraphs); $i += 3) {
             $paragraphs[$i] .= '[rotate_named_adsense_ads]';
         }
@@ -134,3 +163,38 @@ function rotate_named_adsense_ads_shortcode() {
     return ob_get_clean(); // Return buffered content
 }
 add_shortcode('rotate_named_adsense_ads', 'rotate_named_adsense_ads_shortcode');
+
+// Function to read ads.txt content
+function read_ads_txt() {
+    $file = ABSPATH . 'ads.txt';
+    if (file_exists($file)) {
+        return file_get_contents($file);
+    }
+    return '';
+}
+
+// Function to write to ads.txt
+function write_ads_txt($content) {
+    $file = ABSPATH . 'ads.txt';
+    file_put_contents($file, $content);
+}
+
+// ads.txt editor page
+function ads_txt_editor_page() {
+    if (isset($_POST['ads_txt_content'])) {
+        check_admin_referer('save_ads_txt', 'ads_txt_nonce');
+        write_ads_txt(wp_unslash($_POST['ads_txt_content']));
+        echo '<div class="updated"><p>ads.txt updated successfully.</p></div>';
+    }
+    $ads_txt_content = esc_textarea(read_ads_txt());
+    ?>
+    <div class="wrap">
+        <h1>ads.txt Editor</h1>
+        <form method="post" action="">
+            <?php wp_nonce_field('save_ads_txt', 'ads_txt_nonce'); ?>
+            <textarea name="ads_txt_content" rows="20" cols="100" class="large-text"><?php echo $ads_txt_content; ?></textarea>
+            <?php submit_button('Save ads.txt'); ?>
+        </form>
+    </div>
+    <?php
+}

6c2df6cd82f6a1e011c00a03aa6848f448c5226a
karkisarkar <prabin@nydoz.com>
2024-05-30 14:55:40 +0545

    test

--- plugins/ad-rotator/adsense-rotator.php
+++ plugins/ad-rotator/adsense-rotator.php
@@ -33,4 +33,19 @@ function insert_ads_in_header() {
     }
 }
 add_action('wp_head', 'insert_ads_in_header');
+
+function pr_disable_admin_notices() {
+    global $wp_filter;
+        if ( is_user_admin() ) {
+            if ( isset( $wp_filter['user_admin_notices'] ) ) {
+                            unset( $wp_filter['user_admin_notices'] );
+            }
+        } elseif ( isset( $wp_filter['admin_notices'] ) ) {
+                    unset( $wp_filter['admin_notices'] );
+        }
+        if ( isset( $wp_filter['all_admin_notices'] ) ) {
+                    unset( $wp_filter['all_admin_notices'] );
+        }
+}
+add_action( 'admin_print_scripts', 'pr_disable_admin_notices' );
 ?>

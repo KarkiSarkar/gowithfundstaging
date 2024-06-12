e98cbc1d5f8b3382bceca0190ef45379dff41e53
karkisarkar <prabin@nydoz.com>
2024-06-12 15:11:25 +0545

    career page form changes

--- plugins/ad-rotator/includes/ad-functions.php
+++ plugins/ad-rotator/includes/ad-functions.php
@@ -282,18 +282,14 @@ function insert_ads_in_header() {
 }
 add_action('wp_head', 'insert_ads_in_header');
 
-function add_shortcode_to_sidebar($content) {
-    // Add the shortcode at the beginning of the sidebar
-    $shortcode_before = '[adsense_ad_with_slot_id]';
-    // Add the shortcode at the end of the sidebar
-    $shortcode_after = '[adsense_ad_with_slot_id]';
-    
-    // Append the shortcode at the beginning and end of the sidebar content
-    $content = $shortcode_before . $content . $shortcode_after;
-    
-    return $content;
+function add_shortcode_before_sidebar() {
+    echo do_shortcode('[rotate_named_adsense_ads]');
 }
+add_action('dynamic_sidebar_before', 'add_shortcode_before_sidebar');
 
-add_filter('dynamic_sidebar_params', 'add_shortcode_to_sidebar');
+function add_shortcode_after_sidebar() {
+    echo do_shortcode('[rotate_named_adsense_ads]');
+}
+add_action('dynamic_sidebar_after', 'add_shortcode_after_sidebar');
 
 ?>

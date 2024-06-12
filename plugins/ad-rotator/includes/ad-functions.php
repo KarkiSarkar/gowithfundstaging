b99883268e51fa1198629e1ee9da372ddac6e3c0
karkisarkar <prabin@nydoz.com>
2024-06-12 10:01:22 +0545

    career page form changes

--- plugins/ad-rotator/includes/ad-functions.php
+++ plugins/ad-rotator/includes/ad-functions.php
@@ -74,7 +74,7 @@ function display_adsense_ad_unit() {
     
     if ($selected_ad && !is_user_logged_in()) {
         ?>
-        <p><?php echo esc_attr($selected_ad['ad_unit']); ?><div class="slot-id-input"><img src="https://images.unsplash.com/photo-1631270315847-f418bde47ca6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/></div></p>
+        <p><?php echo esc_attr($selected_ad['ad_unit']); ?><div class="slot-id-input"></div></p>
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-<?php echo esc_attr($selected_ad['ad_unit']); ?>&amp;cachebuster=<?php echo time(); ?>" crossorigin="anonymous"></script>
         <?php
     }
@@ -96,6 +96,7 @@ function display_adsense_ad_unit_with_slot_id() {
         <p>
             <?php echo esc_attr($selected_ad['ad_unit']); ?>
             <div class="slot-id-input"><?php echo esc_attr($selected_ad['slot_id']); ?></div>
+            <img width="300" src="https://images.unsplash.com/photo-1631270315847-f418bde47ca6?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"/>
         </p>
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-<?php echo esc_attr($selected_ad['ad_unit']); ?>"
             crossorigin="anonymous"></script>

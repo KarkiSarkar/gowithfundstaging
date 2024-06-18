2cc6b50f19a18ef8146c073faee0b28a0c56b52a
karkisarkar <prabin@nydoz.com>
2024-06-18 14:30:15 +0545

    plugin updated file and added related post in blog single page

--- themes/krowd/footer.php
+++ themes/krowd/footer.php
@@ -72,6 +72,37 @@
   gtag('js', new Date());
   gtag('config', 'G-0ZCYSLL5K1');
 </script>
+<?php
+// Place this code in your theme's footer.php file just before the closing </body> tag
+?>
+
+<!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js"></script>
+
+<!-- Your customer chat code -->
+<div class="fb-customerchat"
+     attribution=setup_tool
+     page_id="1686357274985685">
+</div>
+
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      xfbml            : true,
+      version          : 'v9.0'
+    });
+  };
+
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+</script>
+
 <?php wp_footer(); ?>
 </body>
 </html>

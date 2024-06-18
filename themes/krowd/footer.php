8eea6b402a65e11ce54312ab5da38ed146b7ce49
karkisarkar <prabin@nydoz.com>
2024-06-18 14:38:53 +0545

    career page added

--- themes/krowd/footer.php
+++ themes/krowd/footer.php
@@ -72,33 +72,33 @@
   gtag('js', new Date());
   gtag('config', 'G-0ZCYSLL5K1');
 </script>
-<!-- Load Facebook SDK for JavaScript -->
 <div id="fb-root"></div>
-<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js"></script>
 
-<!-- Your customer chat code -->
-<div class="fb-customerchat"
-     attribution=setup_tool
-     page_id="1686357274985685">
-</div>
+<div id="fb-customer-chat" class="fb-customerchat"></div>
+
+<script>
+  var chatbox = document.getElementById('fb-customer-chat');
+  chatbox.setAttribute("page_id", "PAGE-ID");
+  chatbox.setAttribute("attribution", "biz_inbox");
+</script>
+
 
 <script>
   window.fbAsyncInit = function() {
-    FB.init({
-      xfbml            : true,
-      version          : 'v9.0'
-    });
+	FB.init({
+	  xfbml            : true,
+	  version          : 'API-VERSION'
+	});
   };
 
   (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s); js.id = id;
-    js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-    fjs.parentNode.insertBefore(js, fjs);
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) return;
+	js = d.createElement(s); js.id = id;
+	js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+	fjs.parentNode.insertBefore(js, fjs);
   }(document, 'script', 'facebook-jssdk'));
 </script>
-
 <?php wp_footer(); ?>
 </body>
 </html>

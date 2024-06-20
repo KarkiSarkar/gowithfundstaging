f9a20a866a3441fc2401a1434161182df1abc55a
karkisarkar <prabin@nydoz.com>
2024-06-20 14:50:45 +0545

    test

--- themes/gowithfund/wpcftemplate/woocommerce/basic/single-crowdfunding.php
+++ themes/gowithfund/wpcftemplate/woocommerce/basic/single-crowdfunding.php
@@ -210,13 +210,13 @@ if ( empty( $product ) || ! $product->is_visible() ) {
 }
 ?>
 <div <?php wc_product_class( '', $product ); ?>>
-    <div style="display: flex; justify-content: space-between; width: 30%;" class="container custom-donate-section ">
+    <div style="display: flex; justify-content: space-between; width: 20%;" class="container custom-donate-section ">
         <div style="display: flex;">
             <div style="height: 50px; width: 50px;">
                 <?php echo woocommerce_get_product_thumbnail(); ?>
             </div>
             <div>
-                <p class="woocommerce-loop-product__title" style="width: 50%; font-size: 10px;"><?php echo $product->get_name(); ?></p>
+                <p class="woocommerce-loop-product__title" style="font-size: 10px;"><?php echo $product->get_name(); ?></p>
             </div>
         </div>
         <div>

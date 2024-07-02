43ea0829cc584041bbfff75be8d634cd01c5731e
karkisarkar <prabin@nydoz.com>
2024-07-02 15:21:26 +0545

    test

--- plugins/ad-rotator/includes/ad-functions.php
+++ plugins/ad-rotator/includes/ad-functions.php
@@ -306,7 +306,7 @@ function insert_content_after_third_post() {
         
                 // Check if it's the 3rd post
                   // Check if it's the 3rd post
-                  if ($post_counter == 3 ) {
+                if ($post_counter > 1 && ($post_counter - 2) % 6 == 0) {
                     echo do_shortcode('[adsense_ad_with_slot_id]');
                 }
             }
